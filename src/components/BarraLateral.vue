449cd20e0ff3724991367ca8b9a759eb3c5e6782
Giovanni-Shiroma <giovanni.shiroma04@gmail.com>
2025-03-06 19:48:43 -0300

    first commit

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,55 @@
+<template>
+    <header >
+        <h1>
+            <img src="../assets/logo.png" alt="">
+        </h1>
+        <button class="button" @click="alterarTema">
+
+            {{ textoBotao }}
+        </button>
+    </header>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'BarraLateral',
+        emitis: ['aoTemaAlterado'],
+        data () {
+            return {
+                modoDarkAtivo: false
+            }
+        },
+        computed: {
+            textoBotao () {
+                if(this.modoDarkAtivo) {
+                    return 'Modo light' 
+                }
+                return 'Modo dark'
+            }
+        },
+        methods: {
+            alterarTema() {
+                this.modoDarkAtivo = !this.modoDarkAtivo
+                this.$emit('aoTemaAlterado', this.modoDarkAtivo)
+            }
+        }
+    });
+</script>
+
+<style scoped>
+    header {
+        padding: 1rem;
+        background: #0d3b66;
+        width: 100%;
+        height: 100vh;
+        text-align: center;
+    }
+    @media only screen and (max-width: 768px) {
+    header {
+        padding: 2.5rem;
+        height: auto;
+        }
+    }
+</style>
---
+++ src/components/Box.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="box has-text-weight-bold" :style="estilos">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+import  {defineComponent} from "vue"
+
+export default defineComponent({
+    name: 'BoxTarefas',
+    data () {
+        return {
+            estilos: {
+                background: '#ffcd77'
+            }
+        }
+    }
+})
+</script>
+
+
+<style scoped>
+    /* .box {
+        background: #ffcd77;
+    } */
+</style>
